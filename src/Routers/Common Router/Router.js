d91378cd518f2029cecd49aa605abfcd5214e23e
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-20 06:22:53 +0600

    starting project and basic setup

---
+++ src/Routers/Common Router/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+    
+]);
+
+const Router = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Router;
