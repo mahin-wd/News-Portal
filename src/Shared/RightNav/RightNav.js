b4f6d2c402496d7eab8de6eef0e0a19a89a3540e
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-20 09:07:02 +0600

    sidebar setup

--- src/Shared/RightNav/RightNav.js
+++ src/Shared/RightNav/RightNav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Carouser from '../Carousel/Carousel/Carouser';
+
+
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Ritht Navigation Bar</h2>
+            <ButtonGroup vertical>
+                <Button variant="outline-primary"><FaGoogle/> Login With Google</Button>
+                <Button variant="outline-dark"><FaGithub/> Login With GitHub</Button>
+            </ButtonGroup>
+            <div className='mt-3'>
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facrbook</ListGroup.Item>
+                    <ListGroup.Item><FaWhatsapp/> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <Carouser></Carouser>
+            </div>
         </div>
     );
 };
