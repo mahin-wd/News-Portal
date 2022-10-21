fbadf045a37e41d78f8757de47853b4924bee6bb
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-21 09:49:09 +0600

    news card shown

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../../Shared/NewsCard/NewsCard';
 
 const Home = () => {
-    const news = useLoaderData();
+    const allNews = useLoaderData();
     return (
         <div>
-            <h2>Home: {news.length}</h2>
+            {
+                allNews.map(news => <NewsCard
+                key={news._id}
+                news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
---
+++ src/Shared/NewsCard/NewsCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
+import { FaBookmark, FaShareAlt } from 'react-icons/fa';
+
+const NewsCard = ({news}) => {
+    const {_id, author, details, image_url, title, total_view, rating} = news;
+    console.log(news)
+    return (
+        <Card className="text-center">
+      <Card.Header className='d-flex justify-content-between align-items-center'>
+        <div className='d-flex'>
+          <Image className='me-3' style={{height: '60px'}} roundedCircle src={author.img} />
+          <div className='text-start mt-2'>
+          <h6> <b>{author.name}</b></h6>
+          <p>{author.published_date}</p>
+          </div>
+        </div>
+        <div>
+          <FaShareAlt></FaShareAlt>
+          <FaBookmark></FaBookmark>
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Text>
+          {
+            details.length > 250?
+            <p>{details.slice(0, 250) + '...'}<Link to={`/news/${_id}`}>Read More</Link></p> 
+            :
+            <p>{details}</p>
+          }
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+    );
+};
+
+export default NewsCard;
