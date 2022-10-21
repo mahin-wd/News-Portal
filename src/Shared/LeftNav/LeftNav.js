1fef2736ba9a87002ccc0c729f5bd9c00b5f791a
mahin-wd <wdmahin.mhm@gmail.com>
2022-10-21 06:11:38 +0600

    express server created and url used in fetch

--- src/Shared/LeftNav/LeftNav.js
+++ src/Shared/LeftNav/LeftNav.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/news-categories')
+        .then(res => res.json())
+        .then(data => console.log(data));
+    },[])
+
     return (
         <div>
-            
-            <h2>Left Navigation Bar</h2>
+            <h2></h2>
         </div>
     );
 };
